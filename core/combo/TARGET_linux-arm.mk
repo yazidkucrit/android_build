7f834762480a45bdefdad912e3e82ed60dc3373e
David96 <xd_e-mail@gmx.de>
2013-11-03 17:29:20 +0100

    O3 optimizations + roomservice typo fix

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -35,7 +35,7 @@ TARGET_ARCH_VARIANT := armv5te
 endif
 
 ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
-TARGET_GCC_VERSION := 4.7
+TARGET_GCC_VERSION := 4.8
 else
 TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
@@ -68,16 +68,20 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-TARGET_arm_CFLAGS :=    -O2 \
+TARGET_arm_CFLAGS :=    -O3 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
+						-Wstrict-aliasing=3 \
+						-Werror=strict-aliasing \
                         -funswitch-loops
 
 # Modules can choose to compile some source as thumb.
 TARGET_thumb_CFLAGS :=  -mthumb \
                         -Os \
                         -fomit-frame-pointer \
-                        -fno-strict-aliasing
+                        -fstrict-aliasing \
+						-Wstrict-aliasing=3 \
+						-Werror=strict-aliasing
 
 ifneq ($(filter 4.8 4.8.% 4.9 4.9.%, $(TARGET_GCC_VERSION)),)
 TARGET_arm_CFLAGS +=  -Wno-unused-parameter \
--- tools/roomservice.py
+++ tools/roomservice.py
@@ -193,7 +193,7 @@ def add_to_manifest(repositories, fallback_branch = None, is_dependency = False)
 
         print ('Adding dependecy: %s -> %s' % (repo_full, repo_target))
 
-        if is_dependecy:
+        if is_dependency:
             project = ElementTree.Element("project", attrib = { "path": repo_target,
             "remote": repo_remote, "name": repo_full, "revision": repo_revision })
         else:
