605ccb3b56180f5de9f622b5b61adb83b2b1f779
TheMrcool212 <munirmohamadrani@gmail.com>
2013-11-17 19:11:15 +0800

    Change to -O3

--- core/combo/select.mk
+++ core/combo/select.mk
@@ -48,7 +48,7 @@ $(combo_target)HAVE_KERNEL_MODULES := 0
 
 $(combo_target)GLOBAL_CFLAGS := -fno-exceptions -Wno-multichar
 $(combo_target)RELEASE_CFLAGS := -Os -fno-tree-vectorize -fno-inline-functions -fno-unswitch-loops -fgcse-after-reload -fipa-cp-clone -fpredictive-commoning -fsched-spec-load -fvect-cost-model -g -Wstrict-aliasing=3 -Werror=strict-aliasing
-$(combo_target)GLOBAL_LDFLAGS := -Wl,-O2
+$(combo_target)GLOBAL_LDFLAGS := -Wl,-O3
 $(combo_target)GLOBAL_ARFLAGS := crsP
 
 $(combo_target)EXECUTABLE_SUFFIX :=
