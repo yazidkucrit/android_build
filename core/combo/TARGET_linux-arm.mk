11a8d93c0e917889fb1a445e4fbd328b37fa4104
TheMrcool212 <munirmohamadrani@gmail.com>
2014-03-09 10:01:33 +0800

    Build with -fpic flag

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -146,7 +146,7 @@ endif
 android_config_h := $(call select-android-config-h,linux-arm)
 
 TARGET_GLOBAL_CFLAGS += \
-			-msoft-float $(PIE_GLOBAL_CFLAGS) \
+			-msoft-float -fpic $(PIE_GLOBAL_CFLAGS) \
 			-ffunction-sections \
 			-fdata-sections \
 			-funwind-tables \
