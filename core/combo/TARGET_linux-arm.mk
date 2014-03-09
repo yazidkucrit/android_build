acf56c52c498c9d38d764e82746146dc0b446c37
Paul Beeler <sparksco@gmail.com>
2014-03-09 15:44:34 -0600

    Revert "GCC Freedom"
    This reverts commit a3d72e8f6ffc25e6730e8d32e968bcac6bd47e43.
    
    This breaks build with gcc 4.9.  Regardless if TARGET_GCC_VERSION_AND and TARGET_GCC_VERSION_ARM is set
    the rom still wants to use gcc 4.8
    
    Signed-off-by: Paul Beeler <sparksco@gmail.com>
    
    Conflicts:
    	core/combo/TARGET_linux-arm.mk

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -35,16 +35,10 @@ ifeq ($(strip $(TARGET_ARCH_VARIANT)),)
 TARGET_ARCH_VARIANT := armv5te
 endif
 
-ifeq ($(strip $(TARGET_GCC_VERSION_AND)),)
-TARGET_GCC_VERSION_AND := 4.7
+ifeq ($(strip $(TARGET_GCC_VERSION_EXP)),)
+TARGET_GCC_VERSION := 4.8
 else
-TARGET_GCC_VERSION_AND := $(TARGET_GCC_VERSION_AND)
-endif
-
-ifeq ($(strip $(TARGET_GCC_VERSION_ARM)),)
-TARGET_GCC_VERSION_ARM := 4.7
-else
-TARGET_GCC_VERSION_ARM := $(TARGET_GCC_VERSION_ARM)
+TARGET_GCC_VERSION := $(TARGET_GCC_VERSION_EXP)
 endif
 
 # Specify Target Custom GCC Chains to use:
@@ -70,7 +64,7 @@ include $(TARGET_ARCH_SPECIFIC_MAKEFILE)
 
 # You can set TARGET_TOOLS_PREFIX to get gcc from somewhere else
 ifeq ($(strip $(TARGET_TOOLS_PREFIX)),)
-TARGET_TOOLCHAIN_ROOT := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION_AND)
+TARGET_TOOLCHAIN_ROOT := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_GCC_VERSION)
 TARGET_TOOLS_PREFIX := $(TARGET_TOOLCHAIN_ROOT)/bin/arm-linux-androideabi-
 endif
 
@@ -173,11 +167,9 @@ endif
 # We cannot turn it off blindly since the option is not available
 # in gcc-4.4.x.  We also want to disable sincos optimization globally
 # by turning off the builtin sin function.
-ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.8.% 4.9 4.9.%, $(TARGET_GCC_VERSION_AND)),)
-ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.8.% 4.9 4.9.%, $(TARGET_GCC_VERSION_ARM)),)
+ifneq ($(filter 4.6 4.6.% 4.7 4.7.% 4.8 4.8.% 4.9 4.9.%, $(TARGET_GCC_VERSION)),)
 TARGET_GLOBAL_CFLAGS += -Wno-unused-but-set-variable -fno-builtin-sin \
 			-fno-strict-volatile-bitfields
-endif
 endif
 
 # This is to avoid the dreaded warning compiler message:
